d6db3ed326b55f673a88932730cc34b39cfa37ce
Kevin Villarruel <kevinevolutivos@gmail.com>
2023-02-12 20:04:00 -0500

    Create proyect

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const miBar = document.querySelector(".mi__bar")
+const miMenu = document.querySelector(".mi__manu")
+
+miBar.addEventListener("click", () =>{
+    miMenu.classList.toggle("open__menu")
+})
